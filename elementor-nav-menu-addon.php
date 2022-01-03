5a88ab2bdb2654e7bf34561708dac6c9053857fd
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-01-03 01:30:38 +0100

    Plugin entry point set up

--- elementor-nav-menu-addon.php
+++ elementor-nav-menu-addon.php
@@ -14,34 +14,16 @@
  * @package ENMA
  */
 
- if ( ! defined( 'ABSPATH' ) ) { exit; }
-
-final class Hip_Nav_Addons_Elementor_Pro {
-
-	private static $_instance = null;
-
-	public static function instance() {
-		if ( is_null( self::$_instance ) ) {
-			self::$_instance = new self();
-		}
-		return self::$_instance;
-	}
-
-	public function __construct() {
-		add_action( 'plugins_loaded', [ $this, 'init' ] );
-
-	}
-
-	public function init() {
-		add_action( 'elementor/controls/controls_registered', [ $this, 'init_controls' ] );
-
-	}
-
-	public function init_controls() {
-		require_once( __DIR__ . '/controls/nav-controls.php' );
-        new Hip_Nav_Menu_Addons();
-	}
-
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-Hip_Nav_Addons_Elementor_Pro::instance();
+// Support for site-level autoloading.
+$autoload = __DIR__ . '/vendor/autoload.php';
+
+if ( file_exists( $autoload ) ) {
+	require_once $autoload;
+}
+
+// Initialize plugin loading
+\ENMA\Plugin::init();
