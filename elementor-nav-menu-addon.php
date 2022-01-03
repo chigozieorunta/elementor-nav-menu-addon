e7d59c3123e8f1bd80b4bc14447cc854fa316ade
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-01-03 01:15:50 +0100

    Plugin file changes

--- elementor-nav-menu-addon.php
+++ elementor-nav-menu-addon.php
@@ -1,12 +1,17 @@
 <?php
 /**
- * Plugin Name: Nav Menu Extra Options (Elementor Pro)
- * Description: Extra Options for your Nav Menu on Elementor Pro.
- * Plugin URI:  https://github.com/chigozieorunta
+ * Plugin Name: Elementor Nav Menu Addon
+ * Plugin URI:  https://github.com/elementor-nav-menu-addon
+ * Description: A simple plugin to extend your Elementor Pro Nav Menu.
  * Version:     1.0.0
  * Author:      Chigozie Orunta
  * Author URI:  https://github.com/chigozieorunta
- * Text Domain: hip
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: ENMA
+ * Domain Path: /languages
+ *
+ * @package ENMA
  */
 
  if ( ! defined( 'ABSPATH' ) ) { exit; }
